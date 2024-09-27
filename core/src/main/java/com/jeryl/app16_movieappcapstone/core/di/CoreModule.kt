3fb1fa36ae4d37323591260f4129c4c6205f5b9d
jery lenas <jerylenas@gmail.com>
2024-09-27 12:27:51 +0700

    implementing ssl pinning

--- core/src/main/java/com/jeryl/app16_movieappcapstone/core/di/CoreModule.kt
+++ core/src/main/java/com/jeryl/app16_movieappcapstone/core/di/CoreModule.kt
@@ -11,6 +11,7 @@ import com.jeryl.app16_movieappcapstone.core.domain.repository.IMovieRepository
 import com.jeryl.app16_movieappcapstone.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -56,11 +57,19 @@ val authInterceptor = Interceptor { chain ->
 
     val networkModule = module {
         single {
+            val hostname = "api.themoviedb.org"
+            val certificatePinner = CertificatePinner.Builder()
+                .add(hostname, "sha256/k1Hdw5sdSn5kh/gemLVSQD/P4i4IBQEY1tW4WNxh9XM=")
+                .add(hostname, "sha256/18tkPyr2nckv4fgo0dhAkaUtJ2hu2831xlO2SKhq8dg=")
+                .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+                .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
+                .build()
             OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .addInterceptor(authInterceptor)
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
+                .certificatePinner(certificatePinner)
                 .build()
         }
         single {
